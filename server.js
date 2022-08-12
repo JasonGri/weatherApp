01d0615613f27c64bf1ecbb800c30e99b5068e3c
JasonGri <Iason.Grivas@acg.edu>
2022-08-12 11:50:27 +0300

    Developed initial express app and connected static files & bootstrap

---
+++ server.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const app = require("./src/app");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
+app().listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
---
+++ src/app.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+module.exports = () => {
+  app.set("views", path.join(__dirname, "/views"));
+
+  app.set("view engine", "ejs");
+
+  app.use(express.static(path.join(__dirname, "public")));
+  app.use(
+    "/css",
+    express.static(path.join(__dirname, "../node_modules/bootstrap/dist/css"))
+  );
+  app.use(
+    "/js",
+    express.static(path.join(__dirname, "../node_modules/bootstrap/dist/js"))
+  );
+
+  app.get("/", (req, res) => {
+    res.render("pages/index");
+  });
+
+  return app;
+};
